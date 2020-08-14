70c477ce2dcaed40bac3b9579bd39b5aac897a62
Riya-Anadkat <riya.anadkat@gmail.com>
2020-08-14 11:44:37 -0400

    fixed issues

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ class App extends Component {
         </div> : <button onClick={() => {
             window.location = window.location.href.includes('localhost') 
               ? 'http://localhost:8888/login' 
-              : 'https://better-playlists-backend.herokuapp.com/login' }
+              : 'https://playlist-backend.herokuapp.com/login' }
           }
           style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
         }
