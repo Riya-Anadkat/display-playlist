6cb65f3a06185b4d9f88978579a766fbc304a2b8
Riya-Anadkat <riya.anadkat@gmail.com>
2020-08-19 11:59:27 -0400

    fix playlist

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class Playlist extends Component {
     let playlist = this.props.playlist
     return (
       <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
-        <img src={playlist.imageURL} style = {{width: '60px'}} />
+        <img src={playlist.imageUrl} style={{width: '60px'}}/>
         <h3>{playlist.name}</h3>
         <ul>
           {playlist.songs.map(song => 
